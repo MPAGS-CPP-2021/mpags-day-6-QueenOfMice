3ecacf41a14c6538afd740fb8bf43ef26415ea01
Katharina Lachner <queenofmice41@gmail.com>
2021-11-26 15:57:35 +0000

    Exercise 1 - use exceptions to check if key valid

--- src/MPAGSCipher/CaesarCipher.cpp
+++ src/MPAGSCipher/CaesarCipher.cpp
@@ -15,25 +15,30 @@ CaesarCipher::CaesarCipher(const std::string& key) : key_{0}
     if (!key.empty()) {
         // Before doing the conversion we should check that the string contains a
         // valid positive integer.
-        // Here we do that by looping through each character and checking that it
-        // is a digit. What is rather hard to check is whether the number is too
-        // large to be represented by an unsigned long, so we've omitted that for
-        // the time being.
-        // (Since the conversion function std::stoul will throw an exception if the
-        // string does not represent a valid unsigned long, we could check for and
-        // handle that instead but we only cover exceptions very briefly on the
-        // final day of this course - they are a very complex area of C++ that
-        // could take an entire course on their own!)
-        for (const auto& elem : key) {
-            if (!std::isdigit(elem)) {
+        // check if pos
+        if (key.at(0) == '-') {
                 std::cerr
-                    << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                    << "[error] cipher key must be an unsigned(!) long integer for Caesar cipher,\n"
                     << "        the supplied key (" << key
                     << ") could not be successfully converted" << std::endl;
                 return;
-            }
         }
-        key_ = std::stoul(key) % Alphabet::size;
+        try {
+            key_ = std::stoul(key) % Alphabet::size;
+            // handle cases of too long or invalid key in the following catch
+        } catch (const std::invalid_argument&) {
+            std::cerr
+                << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                << "        the supplied key (" << key
+                << ") could not be successfully converted" << std::endl;
+            return;
+        } catch (const std::out_of_range&) {
+            std::cerr
+                << "[error] cipher key must be an unsigned long integer for Caesar cipher,\n"
+                << "        the supplied key (" << key
+                << ") could not be successfully converted" << std::endl;
+            return;
+        }
     }
 }
 
